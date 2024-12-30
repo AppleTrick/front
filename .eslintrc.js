c58e80ffc44e9c1a60ff70bdf15853f88cd8c6ff
“ParkChangHee” <arrans9591@gmail.com>
2024-12-30 20:32:38 +0900

    이미지 변경 및 elint 변경

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,12 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: ['@react-native', 'prettier'], // prettier 추가
   rules: {
     '@typescript-eslint/no-unused-vars': 'off',
     'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': ['error'], // Prettier 규칙 적용
   },
   parserOptions: {
-    requireConfigFile: false, // Babel 설정 파일을 요구하지 않도록 설정
+    requireConfigFile: false,
   },
 };
